38491f31585874ff541b1452a83176ce69458400
Anastasia <vesheneba2020@gmail.comgit config --global user.email vesheneba2020@gmail.com>
2024-06-04 13:05:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 45_000;
+        int bonus = 20;
+        int mile = price / bonus;
+
+        System.out.println("Итоговый бонус:" + mile);
+    }
+}
+
+
